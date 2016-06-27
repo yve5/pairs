cc2cf6f4f67fccf194827e97dbf42760f74e4ab7
Yve5 <yves.rouille@gmail.com>
2016-06-27 08:42:25 +0200

    micons

--- app/js/app.js
+++ app/js/app.js
@@ -4,11 +4,18 @@ var app = angular.module('app', ['ngRoute']);
 
 app.config(['$routeProvider',
   function ($routeProvider) {
-    $routeProvider.when('/', {
-      templateUrl: 'html/home.html',
-      controller: 'home',
-      controllerAs: 'home'
-    }).otherwise({
-      redirectTo: '/'
-    });
+    $routeProvider
+    	.when('/', {
+	      templateUrl: 'html/game.html',
+	      controller: 'game',
+	      controllerAs: 'game'
+	    })
+    	.when('/home', {
+	      templateUrl: 'html/home.html',
+	      controller: 'home',
+	      controllerAs: 'home'
+	    })
+	    .otherwise({
+	      redirectTo: '/'
+	    });
   }]);
--- app/js/components/game.js
+++ app/js/components/game.js
@@ -2,94 +2,58 @@
 
 var app = angular.module('app');
 
-app.directive('game', ['$timeout', function ($timeout) {
-  return {
-    restrict: 'ACE',
-    require: 'ngModel',
-    templateUrl: 'html/game.html',
-    link: function (scope, elm, attrs, ngModel) {
+app.controller('game', ['$scope', '$http',
+  function (scope, http) {
 
-      scope.init = function () {
-        scope.firstTile   = null;
-        scope.secondTile  = null;
-        scope.canPick     = true;
-        scope.mfTimeout   = 1000;
+  	scope.cells = [
+	    {name:'face'},
+	    {name:'accessibility'},
+	    {name:'extension'},
+	    {name:'fingerprint'},
+	    {name:'favorite'},
+	    {name:'gavel'},
+	    {name:'hourglass_full'},
+	    {name:'motorcycle'},
+	    {name:'polymer'},
+	    {name:'settings_input_hdmi'},
+	    {name:'shopping_cart'},
+	    {name:'thumb_up'},
+	    {name:'work'},
+	    {name:'album'},
+	    {name:'chat_bubble'},
+	    {name:'phone'},
+	    {name:'vpn_key'},
+	    {name:'drafts'},
+	    {name:'weekend'},
+	    {name:'games'},
+	    {name:'radio'},
+	    {name:'remove_circle'},
+	    {name:'sd_card'},
+	    {name:'attach_file'},
+	    {name:'cloud'},
+	    {name:'keyboard'},
+	    {name:'mouse'},
+	    {name:'security'},
+	    {name:'watch'},
+	    {name:'wb_sunny'},
+	    {name:'tag_faces'},
+	    {name:'tune'},
+	    {name:'directions_car'},
+	    {name:'directions_run'},
+	    {name:'directions_subway'},
+	    {name:'map'},
+	    {name:'restaurant'},
+	    {name:'golf_course'},
+	    {name:'cake'},
+	    {name:'public'},
+	    {name:'star'},
+	    {name:'whatshot'},
+	    {name:'local_bar'},
+	    {name:'notifications'},
+	    {name:'school'},
+	    {name:'fitness_center'},
+	    {name:'all_inclusive'},
+	    {name:'beach_access'}
+	  ];
 
-        scope.renderer = new PIXI.autoDetectRenderer(640, 480, {'transparent': true});
-        scope.renderer.backgroundColor = 0xB9F6CA;
-
-        elm.html(scope.renderer.view);
-
-        scope.stage = new PIXI.Container();
-
-        PIXI.loader.add('tiles', 'img/tiles.json').load(scope.loadResources);
-      };
-
-
-      scope.loadResources = function (loader, resources) {
-        // choose 24 random tile images
-        var chosenTiles = new Array();
-        var iii = 0, jjj, kkk;
-
-
-        while (iii < 48) {
-          var randomValue = _.random(0, 43);
-
-          if (_.indexOf(chosenTiles, randomValue) === -1){
-            chosenTiles.push(randomValue, randomValue);
-            iii += 2;
-          }
-        }
-
-
-        // shuffle the chosen tiles
-        for (iii=0; iii<96; iii++){
-          var from  = _.random(0, 47);
-          var to    = _.random(0, 47);
-          var tmp   = chosenTiles[from];
-
-          chosenTiles[from] = chosenTiles[to];
-          chosenTiles[to]   = tmp;
-        }
-
-
-        // place down tiles
-        for (iii = 0; iii < 8; iii++) {
-          for (jjj = 0; jjj < 6; jjj++) {
-            kkk = iii * 6 + jjj;
-
-            var tile = new PIXI.Sprite(resources['tiles'].textures[chosenTiles[kkk]]);
-
-            tile.buttonMode   = true;
-            tile.interactive  = true;
-            tile.isSelected   = false;
-            // tile.tint         = 0x000000;
-            tile.alpha        = 1;
-            tile.position.x   = 7 + iii * 80;
-            tile.position.y   = 7 + jjj * 80;
-
-            tile.mousedown = function (event) {
-              console.log('mousedown', 'fail...');
-            };
-
-            scope.stage.addChild(tile);
-          }
-        }
-
-        scope.animate();
-      }
-
-      scope.animate = function () {
-        // requestAnimationFrame(scope.animate);
-        scope.render();
-      }
-
-      scope.render = function () {
-        // scope.tiles.rotation += 0.01;
-        scope.renderer.render(scope.stage);
-      }
-
-      scope.init();
-    }
-  };
-}]);
+  }]);
