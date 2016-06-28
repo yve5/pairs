b9d26dafa0ccf606cf5f8bddeab389921240b617
yrouille <yrouille@hachette-livre.fr>
2016-06-28 18:59:18 +0200

    find lodash

--- app/js/components/game.js
+++ app/js/components/game.js
@@ -3,61 +3,68 @@
 var app = angular.module('app');
 
 app.controller('game', ['$scope', '$http',
-  function (scope, http) {
+	function (scope, http) {
 
-  	scope.cells = [
-	    {name:'face'},
-	    {name:'accessibility'},
-	    {name:'extension'},
-	    {name:'fingerprint'},
-	    {name:'favorite'},
-	    {name:'gavel'},
-	    {name:'hourglass_full'},
-	    {name:'motorcycle'},
-	    {name:'polymer'},
-	    {name:'settings_input_hdmi'},
-	    {name:'shopping_cart'},
-	    {name:'thumb_up'},
-	    {name:'work'},
-	    {name:'album'},
-	    {name:'chat_bubble'},
-	    {name:'phone'},
-	    {name:'vpn_key'},
-	    {name:'drafts'},
-	    {name:'weekend'},
-	    {name:'games'},
-	    {name:'radio'},
-	    {name:'remove_circle'},
-	    {name:'sd_card'},
-	    {name:'attach_file'},
-	    {name:'cloud'},
-	    {name:'keyboard'},
-	    {name:'mouse'},
-	    {name:'security'},
-	    {name:'watch'},
-	    {name:'wb_sunny'},
-	    {name:'tag_faces'},
-	    {name:'tune'},
-	    {name:'directions_car'},
-	    {name:'directions_run'},
-	    {name:'directions_subway'},
-	    {name:'map'},
-	    {name:'restaurant'},
-	    {name:'golf_course'},
-	    {name:'cake'},
-	    {name:'public'},
-	    {name:'star'},
-	    {name:'whatshot'},
-	    {name:'local_bar'},
-	    {name:'notifications'},
-	    {name:'school'},
-	    {name:'fitness_center'},
-	    {name:'all_inclusive'},
-	    {name:'beach_access'}
-	  ];
+		scope.init = function () {
+	  		scope.canPick = true;
+	  		scope.selection = null;
+	  		scope.firstTile = null;
+	  		scope.secondTile = null;
 
-	  scope.chooseTile = function () {
-	  	console.log('Hello', 'World');
-	  }
+	  		// choose 24 random tile images
+	  		scope.chosenTiles = [];
+	  		var iii = 0;
 
-  }]);
+	  		while (iii < 48) {
+	  			var iconName = scope.icons[_.random(0, scope.icons.length - 1)];
+
+	          	if (_.isUndefined(_.find(scope.chosenTiles, { 'name': iconName }))) {
+					scope.chosenTiles.push({'id' : iii++, 'name' : iconName});
+					scope.chosenTiles.push({'id' : iii++, 'name' : iconName});
+				}
+			}
+
+	        // shuffle the chosen tiles
+	        scope.chosenTiles = _.shuffle(scope.chosenTiles);
+
+	        // _.forEach(scope.chosenTiles, function (item) {
+	        // 	console.log(item);
+	        // })
+
+	    }
+
+	    scope.chooseTile = function (id) {
+
+	    	// if (scope.canPick) {
+	    		console.log('canPick', id, _.isNull(scope.selection));
+
+	    		if (_.isNull(scope.selection)) {
+	    			console.log('selection');
+
+	    			scope.selection = id;
+
+	    			if (_.isNull(scope.firstTile)) {
+	    				scope.firstTile = id;
+	    			}
+	    			else {
+		    			if (_.isNull(scope.secondTile)) {
+		    				scope.secondTile = id;
+		    				console.log('Hello', 'World');
+		    				scope.canPick = false;
+		    			}
+	    			}
+	    		}
+
+	    	// 	scope.canPick = false;
+	    	// }
+
+	    }
+
+
+
+		// Google material glyphicons
+		scope.icons = ['face', 'accessibility', 'extension', 'fingerprint', 'favorite', 'gavel', 'hourglass_full', 'motorcycle', 'polymer', 'settings_input_hdmi', 'shopping_cart', 'thumb_up', 'work', 'album', 'chat_bubble', 'phone', 'vpn_key', 'drafts', 'weekend', 'games', 'radio', 'remove_circle', 'sd_card', 'attach_file', 'cloud', 'keyboard', 'mouse', 'security', 'watch', 'wb_sunny', 'tag_faces', 'tune', 'directions_car', 'directions_run', 'directions_subway', 'map', 'restaurant', 'golf_course', 'cake', 'public', 'star', 'whatshot', 'local_bar', 'notifications', 'school', 'fitness_center', 'all_inclusive', 'beach_access'];
+
+		scope.init();
+
+}]);
