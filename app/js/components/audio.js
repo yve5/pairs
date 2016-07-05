66814e6df7c67ade8b6452e3e51e7c0324ad4cc9
Yve5 <yves.rouille@gmail.com>
2016-07-06 01:19:34 +0200

    ogg gitignore

--- app/js/components/audio.js
+++ app/js/components/audio.js
@@ -2,10 +2,10 @@
 
 var app = angular.module('app');
 
-app.directive('audioPlayer', ['$timeout', function ($timeout) {
+app.directive('audioPlayer', [function () {
   return {
     restrict: 'ACE',
-    template: '<audio controlsa="" loop=""><source src="doc/alone.ogg" type="audio/ogg"></audio>',
+    template: '<audio loop=""><source src="src/alone.ogg" type="audio/ogg"></audio>',
     link: function (scope, elm, attrs, ngModel) {
         var audioTag = elm[0].childNodes[0];
 
