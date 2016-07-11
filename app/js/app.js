a147d085ebac439c5152cc392020923e85bca1b0
yrouille <yrouille@hachette-livre.fr>
2016-07-11 17:50:15 +0200

    ng translate

--- app/js/app.js
+++ app/js/app.js
@@ -1,21 +1,46 @@
 'use strict';
 
-var app = angular.module('app', ['ngRoute']);
+var app = angular.module('app', ['ngRoute', 'pascalprecht.translate']);
 
 app.config(['$routeProvider',
   function ($routeProvider) {
     $routeProvider
-        .when('/', {
-          templateUrl: 'html/game.html',
-          controller: 'game',
-          controllerAs: 'game'
-        })
-    	.when('/help', {
-	      templateUrl: 'html/help.html',
-	      controller: 'help',
-	      controllerAs: 'help'
-	    })
-	    .otherwise({
-	      redirectTo: '/'
-	    });
+    .when('/', {
+      templateUrl: 'html/game.html',
+      controller: 'game',
+      controllerAs: 'game'
+    })
+    .when('/help', {
+      templateUrl: 'html/help.html',
+      controller: 'help',
+      controllerAs: 'help'
+    })
+    .otherwise({
+      redirectTo: '/help'
+    });
   }]);
+
+
+
+
+app.config(['$translateProvider', function ($translateProvider) {
+  $translateProvider.translations('en', enVal);
+  $translateProvider.translations('de', deVal);
+  $translateProvider.preferredLanguage('de');
+}]);
+
+
+
+
+var enVal = {
+  'title': 'Welcome',
+  'foo': 'This is a paragraph',
+  'hello': 'Hello World'
+};
+
+
+var deVal = {
+  'title': 'Hallo',
+  'foo': 'Dies ist ein Absatz',
+  'hello': 'Hello World'
+};
